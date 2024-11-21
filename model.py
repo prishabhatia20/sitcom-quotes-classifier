a9492811abfe7901ccb050f752d86c15461bd568
prishabhatia20 <prishabhatia@gmail.com>
2024-11-21 11:06:58 -0500

    clean data

--- model.py
+++ model.py
@@ -1,2 +1,18 @@
-import spacy_sentence_bert
+import pandas as pd
 
+
+# Loading and cleaning Friends data
+friends_data = pd.read_csv("datasets/friends_quotes.csv.zip", compression='zip')
+friends_data.drop(['episode_number', 'episode_title', 'quote_order', 'season'], axis=1, inplace=True)
+
+# Load The Office quotes
+
+office_monologue = pd.read_csv("datasets/talking_head.csv")
+office_response = pd.read_csv("datasets/parent_reply.csv.zip", compression='zip')
+
+office_monologue.drop(['quote_id'], axis=1, inplace=True)
+office_response.drop(['parent_id', 'parent'], axis=1, inplace=True)
+office_response.rename(columns={'reply': 'quote'}, inplace=True)
+
+office_data = pd.concat([office_monologue, office_response]).reset_index(drop=True)
+print(office_data)
