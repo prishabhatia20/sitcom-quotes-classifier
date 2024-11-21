7dc763d90a8806fde746287fae4ff6ea2e134f74
prishabhatia20 <prishabhatia@gmail.com>
2024-11-21 11:28:08 -0500

    load and clean brooklyn 99 data

--- model.py
+++ model.py
@@ -4,6 +4,8 @@ import pandas as pd
 # Loading and cleaning Friends data
 friends_data = pd.read_csv("datasets/friends_quotes.csv.zip", compression='zip')
 friends_data.drop(['episode_number', 'episode_title', 'quote_order', 'season'], axis=1, inplace=True)
+friends_data.rename(columns={'author': 'character'}, inplace=True)
+print(friends_data)
 
 # Load The Office quotes
 
@@ -16,3 +18,8 @@ office_response.rename(columns={'reply': 'quote'}, inplace=True)
 
 office_data = pd.concat([office_monologue, office_response]).reset_index(drop=True)
 print(office_data)
+# Load Brooklyn 99 quotes
+
+brooklyn_data = pd.read_csv("datasets/Brooklyn99_Season1-4_Dataset.csv")
+brooklyn_data.rename(columns={'name': 'character', 'line': 'quote'}, inplace=True)
+print(brooklyn_data)
