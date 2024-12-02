8a8bc52ea875c2c8352e2da58adbb1ffb78c6995
prishabhatia20 <prishabhatia@gmail.com>
2024-12-02 11:34:25 -0500

    refactor code & create model

--- model.py
+++ model.py
@@ -1,25 +1,25 @@
-import pandas as pd
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras import layers
+# import keras
+from constants import NUM_WORDS, MAX_LEN, NUM_CLASSES
 
+class Model(tf.keras.Model):
+    def __init__(self, vocab_dim=NUM_WORDS, max_len=MAX_LEN, num_classes=NUM_CLASSES):
+        super(Model, self).__init__()
+        self.embedding = layers.Embedding(vocab_dim, 32, input_length=max_len)
+        self.lstm1 = layers.Bidirectional(layers.LSTM(32, return_sequences=True))
+        self.lstm2 = layers.Bidirectional(layers.LSTM(16))
+        self.dense = layers.Dense(64, activation='relu')
+        self.dropout = layers.Dropout(0.5)
+        self.classifier = layers.Dense(num_classes, activation='softmax')
 
-# Loading and cleaning Friends data
-friends_data = pd.read_csv("datasets/friends_quotes.csv.zip", compression='zip')
-friends_data.drop(['episode_number', 'episode_title', 'quote_order', 'season'], axis=1, inplace=True)
-friends_data.rename(columns={'author': 'character'}, inplace=True)
-print(friends_data)
-
-# Load The Office quotes
-
-office_monologue = pd.read_csv("datasets/talking_head.csv")
-office_response = pd.read_csv("datasets/parent_reply.csv.zip", compression='zip')
-
-office_monologue.drop(['quote_id'], axis=1, inplace=True)
-office_response.drop(['parent_id', 'parent'], axis=1, inplace=True)
-office_response.rename(columns={'reply': 'quote'}, inplace=True)
-
-office_data = pd.concat([office_monologue, office_response]).reset_index(drop=True)
-print(office_data)
-# Load Brooklyn 99 quotes
-
-brooklyn_data = pd.read_csv("datasets/Brooklyn99_Season1-4_Dataset.csv")
-brooklyn_data.rename(columns={'name': 'character', 'line': 'quote'}, inplace=True)
-print(brooklyn_data)
+    def call(self, inputs, training=False):
+        x = self.embedding(inputs)
+        x = self.lstm1(x)
+        x = self.lstm2(x)
+        x = self.dense(x)
+        if training:
+            x = self.dropout(x, training=training)
+        return self.classifier(x)
