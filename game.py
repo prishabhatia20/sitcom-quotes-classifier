c0ccfa4604b32dc4ee39680553892d5659d3388e
prishabhatia20 <prishabhatia@gmail.com>
2024-12-03 18:24:42 -0500

    Initial game setup

--- game.py
+++ game.py
@@ -1,7 +1,12 @@
 import pygame
 from constants import WIDTH, HEIGHT
+import os
 import time
 pygame.init()
+
+main_screen = pygame.image.load(
+        os.path.join("images", "background.png")
+    ).convert()
 
 screen = pygame.display.set_mode((HEIGHT, WIDTH))
 clock = pygame.time.Clock()
