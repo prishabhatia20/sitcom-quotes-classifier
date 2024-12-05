d730c374c0e294a4c8d3b4337a9ac18aac17df7a
Mihir Vemuri <mvemuri@olin.edu>
2024-12-05 04:16:44 -0500

    Attempt 1 at model training code. Code did not compile.

--- helpers.py
+++ helpers.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-
 from constants import NUM_WORDS, MAX_LEN, NUM_CLASSES
+import pickle  # For saving and loading tokenizer
 
 def tokenize_and_sequence(train_sentences,
                           test_sentences,
@@ -24,3 +24,46 @@ def tokenize_and_sequence(train_sentences,
     )
 
     return train_sequences, test_sequences, tokenizer
+
+def save_tokenizer(tokenizer, filepath):
+    with open(filepath, 'wb') as file:
+        pickle.dump(tokenizer, file)
+
+def load_tokenizer(filepath):
+    with open(filepath, 'rb') as file:
+        return pickle.load(file)
+
+def preprocess_text(sentences, tokenizer=None, maxlen=MAX_LEN, num_words=NUM_WORDS, train_mode=False):
+    """
+    adds code to -prepare raw text data for the mdoel
+    """
+    if tokenizer is None and train_mode:
+        tokenizer = Tokenizer(num_words=num_words, oov_token='<OOV>')
+        tokenizer.fit_on_texts(sentences)
+    elif tokenizer is None:
+        raise ValueError("Tokenizer must be provided when not in train mode.")
+    
+    sequences = tokenizer.texts_to_sequences(sentences)
+    sequences = pad_sequences(sequences, padding='post', maxlen=maxlen, truncating='post')
+    return sequences, tokenizer
+
+def get_class_weights(labels, num_classes=NUM_CLASSES):
+    """
+    if theres a class imablance, we get the weights of the class
+    """
+    class_counts = [sum(1 for label in labels if label == i) for i in range(num_classes)]
+    total = sum(class_counts)
+    class_weights = {i: total / (num_classes * class_counts[i]) for i in range(num_classes) if class_counts[i] > 0}
+    return class_weights
+
+def split_and_preprocess_data(data, tokenizer=None, train_ratio=0.8, num_words=NUM_WORDS, maxlen=MAX_LEN):
+    """
+    split data into train and test sets, thenpreprocess and return the sequences and tokenizer.
+    """
+    from sklearn.model_selection import train_test_split
+    
+    train_data, test_data = train_test_split(data, train_size=train_ratio, stratify=data['character'])
+    train_sequences, test_sequences, tokenizer = tokenize_and_sequence(
+        train_data['quote'], test_data['quote'], num_words=num_words, maxlen=maxlen
+    )
+    return train_sequences, train_data['character'], test_sequences, test_data['character'], tokenizer
--- model.py
+++ model.py
@@ -1,17 +1,15 @@
 import tensorflow as tf
-# from tensorflow.keras.preprocessing.text import Tokenizer
-# from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras import layers
-# import keras
+from helpers import preprocess_text, get_class_weights, split_and_preprocess_data, save_tokenizer, load_tokenizer
 from constants import NUM_WORDS, MAX_LEN, NUM_CLASSES
 
 class Model(tf.keras.Model):
     def __init__(self, vocab_dim=NUM_WORDS, max_len=MAX_LEN, num_classes=NUM_CLASSES):
         super(Model, self).__init__()
-        self.embedding = layers.Embedding(vocab_dim, 32, input_length=max_len)
-        self.lstm1 = layers.Bidirectional(layers.LSTM(32, return_sequences=True))
-        self.lstm2 = layers.Bidirectional(layers.LSTM(16))
-        self.dense = layers.Dense(64, activation='relu')
+        self.embedding = layers.Embedding(vocab_dim, 128, input_length=max_len)
+        self.lstm1 = layers.Bidirectional(layers.LSTM(64, return_sequences=True))
+        self.lstm2 = layers.Bidirectional(layers.LSTM(32))
+        self.dense = layers.Dense(128, activation='relu')
         self.dropout = layers.Dropout(0.5)
         self.classifier = layers.Dense(num_classes, activation='softmax')
 
@@ -23,3 +21,29 @@ class Model(tf.keras.Model):
         if training:
             x = self.dropout(x, training=training)
         return self.classifier(x)
+
+
+def train_model(data, model_save_path, tokenizer_save_path, train_ratio=0.8, batch_size=64, epochs=20):
+    train_sequences, train_labels, test_sequences, test_labels, tokenizer = split_and_preprocess_data(data, train_ratio=train_ratio)
+    save_tokenizer(tokenizer, tokenizer_save_path)
+    class_weights = get_class_weights(train_labels)
+    model = Model()
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    model.fit(
+        train_sequences,
+        train_labels,
+        validation_data=(test_sequences, test_labels),
+        class_weight=class_weights,
+        batch_size=batch_size,
+        epochs=epochs
+    )
+    model.save(model_save_path)
+    return model
+
+
+def predict_quotes(quotes, model_path, tokenizer_path):
+    model = tf.keras.models.load_model(model_path)
+    tokenizer = load_tokenizer(tokenizer_path)
+    sequences, _ = preprocess_text(quotes, tokenizer=tokenizer, maxlen=MAX_LEN, train_mode=False)
+    predictions = model.predict(sequences)
+    return predictions.argmax(axis=1)
